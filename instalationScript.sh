8b7c9007e6a0a8f0b74e3f83d35ce828c3cdb0f1
Alonso Rubio <alonsorafael_130798@hotmail.com>
2021-12-06 15:49:41 -0600

    Update instalationScript.sh

--- instalationScript.sh
+++ instalationScript.sh
@@ -7,6 +7,8 @@ fi
 
 apt install libsdl2-dev libgtkmm-3.0-dev libportaudio2 python3-gi libopenjp2-7 git ninja-build xorg python3-pip meson -y
 
+pip3 install -r requirements.txt
+
 git clone https://github.com/snes9xgit/snes9x.git
 
 cd snes9x
