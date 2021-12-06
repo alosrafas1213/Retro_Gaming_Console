dc36348c463a734fb4bf8549eeff48dab02608f6
Alonso <alosrafas@gmail.com>
2021-12-06 21:12:11 +0000

    Instalation Script Added

---
+++ instalationScript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+apt install libsdl2-dev libgtkmm-3.0-dev libportaudio2 python3-gi libopenjp2-7 git ninja xorg pip3 meson -y
+
+git clone https://github.com/snes9xgit/snes9x.git
+
+cd snes9x/gtk
+
+meson build --prefix=/usr --buildtype=release --strip
+
+cd build
+
+ninja
+
+ninja install
